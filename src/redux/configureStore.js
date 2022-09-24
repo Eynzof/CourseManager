4ffd52851da62d1b378243ca97d98ec30f8fc074
eynzo <Eynzof@gmail.com>
2022-09-24 13:35:36 +0800

    production builds

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,13 +1,6 @@
-import { applyMiddleware, compose, createStore } from 'redux';
-import rootReducer from './reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import thunk from 'redux-thunk';
-
-
-export default function configureStore(initialState) {
-    // add support for redux devtools
-    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-    // 这里的reduxImmutableStateInvariant()很重要
-    // 这个中间件会在你修改state时提醒你
-    return createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant())));
+if(process.env.NODE_ENV === "production") {
+    module.exports = require('./configureStore.prod')
+} else {
+    module.exports = require('./configureStore.dev')
 }
+ 
